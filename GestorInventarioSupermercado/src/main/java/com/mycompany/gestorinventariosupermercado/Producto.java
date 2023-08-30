c6acb7048cc06a4bb638994e2ab1f131658d25df
fabipv <fabianapinavera13@gmail.com>
2023-08-29 22:48:27 -0400

    Update Producto class

--- GestorInventarioSupermercado/src/main/java/com/mycompany/gestorinventariosupermercado/Producto.java
+++ GestorInventarioSupermercado/src/main/java/com/mycompany/gestorinventariosupermercado/Producto.java
@@ -1,106 +1,71 @@
 package com.mycompany.gestorinventariosupermercado;
 
-import java.time.LocalDate;
-
 /**
  *
  * @author mati1
  */
-public class Producto {
-    private String nombre;
-    private String codigoBarra;
-    private String descripcion;
-    private double precio;
-    private String categoria;
-    private int cantidadStock;
-    private LocalDate fechaVencimiento; // Si es un producto perecedero
 
+public class Producto {
+    private String nombreProducto;
+    private String codigoBarra;
+    private double precio;
+    private int cantidadStock;
+  
     // Constructor
-    public Producto(String nombre, String codigoBarra, String descripcion, double precio, String categoria,
-            int cantidadStock, LocalDate fechaVencimiento) {
-        this.nombre = nombre;
+    public Producto(String nombreProducto, String codigoBarra,double precio, int cantidadStock) {
+        this.nombreProducto = nombreProducto;
         this.codigoBarra = codigoBarra;
-        this.descripcion = descripcion;
         this.precio = precio;
-        this.categoria = categoria;
         this.cantidadStock = cantidadStock;
-        this.fechaVencimiento = fechaVencimiento;
     }
 
     // Getters
     public String getNombre() {
-        return this.nombre;
+        return this.nombreProducto;
     }
 
     public String getCodigoBarra() {
         return this.codigoBarra;
     }
 
-    public String getDescripcion() {
-        return this.descripcion;
-    }
-
     public double getPrecio() {
         return this.precio;
-    }
-
-    public String getCategoria() {
-        return this.categoria;
     }
 
     public int getCantidadStock() {
         return this.cantidadStock;
     }
 
-    public LocalDate getFechaVencimiento() {
-        return this.fechaVencimiento;
-    }
-
     // Setters
     public void setNombre(String nombre) {
-        this.nombre = nombre;
+        this.nombreProducto = nombre;
     }
 
     public void setCodigoBarra(String codigoBarra) {
         this.codigoBarra = codigoBarra;
     }
 
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
-
     public void setPrecio(double precio) {
         this.precio = precio;
-    }
-
-    public void setCategoria(String categoria) {
-        this.categoria = categoria;
     }
 
     public void setCantidadStock(int cantidadStock) {
         this.cantidadStock = cantidadStock;
     }
 
-    public void setFechaVencimiento(LocalDate fechaVencimiento) {
-        this.fechaVencimiento = fechaVencimiento;
+
+    // Métodos de sobrecarga
+
+    // Sobrecarga del método para actualizar el stock
+    public void actualizarStock(int nuevaCantidad) {
+        this.cantidadStock = nuevaCantidad;
     }
 
-    // Otros métodos
-    public void aumentarStock(int cantidad) {
-        this.cantidadStock += cantidad;
-    }
-
-    public void disminuirStock(int cantidad) {
-        this.cantidadStock -= cantidad;
-    }
-
-    public void mostrarDatos() {
-        System.out.println("Nombre: " + this.nombre);
-        System.out.println("Código de barra: " + this.codigoBarra);
-        System.out.println("Descripción: " + this.descripcion);
-        System.out.println("Precio: " + this.precio);
-        System.out.println("Categoría: " + this.categoria);
-        System.out.println("Cantidad en stock: " + this.cantidadStock);
-        System.out.println("Fecha de vencimiento: " + this.fechaVencimiento);
+    public void actualizarStock(int nuevaCantidad, boolean aumentar) {
+        if (aumentar) {
+            this.cantidadStock += nuevaCantidad;
+        } else {
+            this.cantidadStock -= nuevaCantidad;
+        }
     }
 }
