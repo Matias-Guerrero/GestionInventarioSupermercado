64a0cc6da24d2ac985f4530a0629ef3c9a8f0607
Matias Guerrero Castillo <mati.guerrerocastillo@gmail.com>
2023-08-31 19:29:01 -0400

    Update Gestor Class and GestorInventarioSupermercado Class

--- GestorInventarioSupermercado/src/main/java/com/mycompany/gestorinventariosupermercado/Gestor.java
+++ GestorInventarioSupermercado/src/main/java/com/mycompany/gestorinventariosupermercado/Gestor.java
@@ -3,10 +3,6 @@ package com.mycompany.gestorinventariosupermercado;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-/**
- *
- * @author mati1
- */
 public class Gestor {
     private ArrayList<Proveedor> proveedores;
     private HashMap<String, Producto> mapaProductos;
@@ -18,33 +14,81 @@ public class Gestor {
     }
     
     // Metodos
-    public void agregarProveedor(Proveedor proveedor) {
-        this.proveedores.add(proveedor);
-
-        // Actualiza el HashMap de productos con los productos del nuevo proveedor
-        for (Producto producto : proveedor.getProductosSuministrados()) {
-            mapaProductos.put(producto.getCodigoBarra(), producto);
+    public void agregarProveedor(String nombre, String correo) {
+        Proveedor busquedaProveedor = this.buscarProveedor(nombre);
+        
+        if(busquedaProveedor != null){
+            Proveedor proveedor = new Proveedor(nombre, correo);
+            
+            this.proveedores.add(proveedor);
+            
+            System.out.println("El Proveedor se agrego exitosamente a la base de datos.");
         }
-    }
-
-    public void eliminarProveedor(String nombre) {
-        Proveedor proveedor = this.buscarProveedor(nombre);
-        if (proveedor != null) {
-            this.proveedores.remove(proveedor);
-
-            // Elimina los productos del proveedor del HashMap de productos
-            for (Producto producto : proveedor.getProductosSuministrados()) {
-                mapaProductos.remove(producto.getCodigoBarra());
-            }
+        else {
+            System.out.print("El Proveedor ya existe en la base de datos.");
         }
     }
     
+    // Metodo de Sobrecarga
+    public void agregarProveedor(String nombre, String correo, ArrayList<Producto> productos) {
+        Proveedor busquedaProveedor = this.buscarProveedor(nombre);
+
+        if(busquedaProveedor != null){
+            Proveedor proveedor = new Proveedor(nombre, correo);
+            
+            this.proveedores.add(proveedor);
+            
+            System.out.println("El Proveedor se agrego exitosamente a la base de datos.");
+
+            // Actualiza el HashMap de productos con los productos del nuevo proveedor y si ya existen, actualiza el stock
+            for (Producto producto : productos) {
+                Producto busquedaProducto = this.mapaProductos.get(producto.getCodigoBarra());
+                if (busquedaProducto != null) {
+                    busquedaProducto.actualizarStock(producto.getCantidadStock(), true);
+                }
+                else {
+                    this.mapaProductos.put(producto.getCodigoBarra(), producto);
+                }
+            }
+        }
+        else {
+            System.out.print("El Proveedor ya existe en la base de datos.");
+        }
+    }
+
+    public Proveedor eliminarProveedor(String nombre) {
+        Proveedor proveedor = this.buscarProveedor(nombre);
+
+        if (proveedor != null) {
+            this.proveedores.remove(proveedor);
+
+            // Elimina los productos del proveedor del HashMap de productos y actualiza el stock, si el producto llega a 0, lo elimina del HashMap
+            for (Producto producto : proveedor.getProductosSuministrados()) {
+                Producto busquedaProducto = this.mapaProductos.get(producto.getCodigoBarra());
+                
+                if (busquedaProducto != null) {
+                    busquedaProducto.actualizarStock(producto.getCantidadStock(), false);
+                    
+                    if (busquedaProducto.getCantidadStock() == 0) {
+                        this.mapaProductos.remove(producto.getCodigoBarra());
+                    }
+                }
+            }
+
+            return proveedor;
+        }
+
+        return null;
+    }
+    
     public Proveedor buscarProveedor(String nombre) {
+        
         for (Proveedor proveedor : this.proveedores) {
             if (proveedor.getNombre().equals(nombre)) {
                 return proveedor;
             }
         }
+        
         return null;
     }
 }
--- GestorInventarioSupermercado/src/main/java/com/mycompany/gestorinventariosupermercado/GestorInventarioSupermercado.java
+++ GestorInventarioSupermercado/src/main/java/com/mycompany/gestorinventariosupermercado/GestorInventarioSupermercado.java
@@ -5,8 +5,19 @@ import java.io.*;
 public class GestorInventarioSupermercado {
     // Metodos Auxiliares
     public static void limpiarPantalla() {
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
+        try {
+            final String os = System.getProperty("os.name");
+            System.out.println(os);
+            if (os.contains("Windows")) {  
+              Runtime.getRuntime().exec("cls");  
+            }
+            else {  
+              Runtime.getRuntime().exec("clear");  
+            } 
+        }  
+        catch (final Exception e) {  
+          e.printStackTrace();  
+        }
     }
 
     public static void pausar() {
@@ -22,143 +33,68 @@ public class GestorInventarioSupermercado {
         BufferedReader lector = new BufferedReader(new InputStreamReader(System.in));
         Gestor gestor = new Gestor(); // Crea una instancia del Gestor
         
-        int opcionPrincipal;
-
+        int opcion;
+        
         do {
             limpiarPantalla();
             System.out.println("=============================================");
-            System.out.println("Sistema de Gestión de Productos y Proveedores");
+            System.out.println("Sistema de Gestion de Proveedores y Productos");
             System.out.println("=============================================");
-            System.out.println("Seleccione una opción:");
-            System.out.println("1) Trabajar con Proveedores");
-            System.out.println("2) Trabajar con Productos");
-            System.out.println("3) Salir");
+            System.out.println("1) Agregar Proveedor");
+            System.out.println("2) Eliminar Proveedor");
+            System.out.println("3) Buscar Proveedor");
+            System.out.println("4) Lista de Productos del Proveedor");
+            System.out.println("5) Mostrar Productos y su stock");
+            System.out.println("6) Salir");
+            System.out.println("=============================================");
             System.out.print("Opción: ");
-            
-            opcionPrincipal = Integer.parseInt(lector.readLine());
-            
-            switch (opcionPrincipal) {
+
+            opcion = Integer.parseInt(lector.readLine());
+
+            switch (opcion) {
                 case 1:
-                    // Menú de Proveedores
-                    int opcionProveedor;
-                    do {
-                        limpiarPantalla();
-                        System.out.println("=============================================");
-                        System.out.println("Menú de Proveedores");
-                        System.out.println("=============================================");
-                        System.out.println("1) Agregar Proveedor");
-                        System.out.println("2) Eliminar Proveedor");
-                        System.out.println("3) Buscar Proveedor");
-                        System.out.println("4) Mostrar Lista de Productos Suministrados");
-                        System.out.println("5) Regresar al Menú Principal");
-                        System.out.print("Opción: ");
-                        
-                        opcionProveedor = Integer.parseInt(lector.readLine());
-                        
-                        switch (opcionProveedor) {
-                            case 1:
-                                // ================================
-                                // Agregar Proveedor
-                                // ================================
+                    // ================================
+                    // Agregar Proveedor
+                    // ================================
 
-                                break;
-                            case 2:
-                                // ================================
-                                // Eliminar Proveedor
-                                // ================================
-
-                                break;
-                            case 3:
-                                // ================================
-                                // Buscar Proveedor
-                                // ================================
-                            
-                                break;
-                            case 4:
-                                // ========================================
-                                // Mostrar Lista de Productos Suministrados
-                                // ========================================
-
-                                break;
-                            case 5:
-                                // Regresar al Menú Principal
-                                break;
-                            default:
-                                System.out.println("Opción inválida.");
-                                break;
-                        }
-                        
-                        System.out.println();
-                        pausar();
-                        lector.readLine(); // Limpia el buffer del lector
-
-                    } while (opcionProveedor != 5);
                     break;
-
                 case 2:
-                    // Menú de Productos
-                    int opcionProducto;
-                    do {
-                        limpiarPantalla();
-                        System.out.println("=============================================");
-                        System.out.println("Menú de Productos");
-                        System.out.println("=============================================");
-                        System.out.println("1) Agregar Producto");
-                        System.out.println("2) Eliminar Producto");
-                        System.out.println("3) Buscar Producto");
-                        System.out.println("4) Mostrar Productos y su stock");
-                        System.out.println("5) Regresar al Menú Principal");
-                        System.out.print("Opción: ");
-                        
-                        opcionProducto = Integer.parseInt(lector.readLine());
-                        
-                        switch (opcionProducto) {
-                            case 1:
-                                // Agregar Producto
-                                // ... (Código para agregar producto)
-                                break;
-                            case 2:
-                                // Eliminar Producto
-                                // ... (Código para eliminar producto)
-                                break;
-                            case 3:
-                                // Buscar Producto
-                                // ... (Código para buscar producto)
-                                break;
-                            case 4:
-                                // Mostrar Productos y su stock
-                                // ... (Código para mostrar productos y su stock)
-                                break;
-                            case 5:
-                                // Regresar al Menú Principal
-                                break;
-                            default:
-                                System.out.println("Opción inválida.");
-                                break;
-                        }
-                        
-                        System.out.println();
-                        pausar();
-                        lector.readLine(); // Limpia el buffer del lector
+                    // ================================
+                    // Eliminar Proveedor
+                    // ================================
 
-                    } while (opcionProducto != 5);
                     break;
-
                 case 3:
-                    // Salir
-                    System.out.println("Saliendo del programa...");
-                    break;
+                    // ================================
+                    // Buscar Proveedor
+                    // ================================
 
+                    break;
+                case 4:
+                    // ========================================
+                    // Mostrar Lista de Productos del Proveedor
+                    // ========================================
+
+                    break;
+                case 5:
+                    // ========================================
+                    // Mostrar Productos y su Stock
+                    // ========================================
+                    
+                    break;
+                case 6:
+                    // ========================================
+                    // Salir
+                    // ========================================                    
+                    break;
                 default:
                     System.out.println("Opción inválida.");
                     break;
             }
-            
-            System.out.println();
-            pausar();
-            lector.readLine(); // Limpia el buffer del lector
 
-        } while (opcionPrincipal != 3);
+            pausar();
+
+        } while (opcion != 6);
 
         lector.close();
     }
