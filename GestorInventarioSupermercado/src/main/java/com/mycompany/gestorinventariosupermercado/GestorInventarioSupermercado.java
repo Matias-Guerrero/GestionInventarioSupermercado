32111f57c3c27e0663088390212aef078374a82a
samira <samira.becerra.s@mail.pucv.cl>
2023-09-01 16:25:03 -0400

    Update Menu

--- GestorInventarioSupermercado/src/main/java/com/mycompany/gestorinventariosupermercado/GestorInventarioSupermercado.java
+++ GestorInventarioSupermercado/src/main/java/com/mycompany/gestorinventariosupermercado/GestorInventarioSupermercado.java
@@ -29,21 +29,70 @@ public class GestorInventarioSupermercado {
         }
     }
 
-    public static void main(String[] args) throws IOException {
+    // Metodo para agregar datos iniciales al gestor
+    public static void cargarDatosIniciales(Gestor gestor) {
+        // Crear proveedores
+        Proveedor proveedor1 = new Proveedor("Proveedor A", "proveedora@example.com");
+        Proveedor proveedor2 = new Proveedor("Proveedor B", "proveedorb@example.com");
+        Proveedor proveedor3 = new Proveedor("Proveedor C", "proveedorc@example.com");
+    
+        // Crear productos
+        Producto producto1 = new Producto("Manzanas", 2500, 200);
+        Producto producto2 = new Producto("Plátanos", 1800, 150);
+        Producto producto3 = new Producto("Naranjas", 3000, 100);
+        Producto producto4 = new Producto("Leche", 1000, 300);
+        Producto producto5 = new Producto("Huevos", 2200, 250);
+        Producto producto6 = new Producto("Pan", 1500, 200);
+        Producto producto7 = new Producto("Arroz", 2000, 400);
+        Producto producto8 = new Producto("Fideos", 1800, 350);
+        Producto producto9 = new Producto("Gomitas", 500, 1000);
+    
+        // Asociar productos a proveedores
+        proveedor1.agregarProductoSuministrado(producto1);
+        proveedor1.agregarProductoSuministrado(producto2);
+        proveedor2.agregarProductoSuministrado(producto2);
+        proveedor2.agregarProductoSuministrado(producto3);
+        proveedor3.agregarProductoSuministrado(producto4);
+        proveedor3.agregarProductoSuministrado(producto5);
+        proveedor3.agregarProductoSuministrado(producto6);
+        proveedor3.agregarProductoSuministrado(producto7);
+        proveedor3.agregarProductoSuministrado(producto8);
+        proveedor3.agregarProductoSuministrado(producto9);
+    
+        // Agregar proveedores y productos al gestor
+        gestor.getProveedores().add(proveedor1);
+        gestor.getProveedores().add(proveedor2);
+        gestor.getProveedores().add(proveedor3);
+    
+        gestor.getMapaProductos().put(producto1.getNombre(), producto1);
+        gestor.getMapaProductos().put(producto2.getNombre(), producto2);
+        gestor.getMapaProductos().put(producto3.getNombre(), producto3);
+        gestor.getMapaProductos().put(producto4.getNombre(), producto4);
+        gestor.getMapaProductos().put(producto5.getNombre(), producto5);
+        gestor.getMapaProductos().put(producto6.getNombre(), producto6);
+        gestor.getMapaProductos().put(producto7.getNombre(), producto7);
+        gestor.getMapaProductos().put(producto8.getNombre(), producto8);
+        gestor.getMapaProductos().put(producto9.getNombre(), producto9);
+    }
+
+    public static void main(String[] args) throws IOException, NumberFormatException {
         BufferedReader lector = new BufferedReader(new InputStreamReader(System.in));
         Gestor gestor = new Gestor(); // Crea una instancia del Gestor
+
+        // Cargar datos iniciales
+        cargarDatosIniciales(gestor);
         
-        int opcion;
-        
-        do {
+        while (true) {
+            int opcion;
+
             limpiarPantalla();
             System.out.println("=============================================================");
-            System.out.println("Sistema de Gestion de Productos suministrados por Proveedores");
+            System.out.println("Sistema de Gestion de Productos Suministrados por Proveedores");
             System.out.println("=============================================================");
-            System.out.println("1) Agregar Producto en Proveedor");
-            System.out.println("2) Eliminar Producto en Proveedor");
-            System.out.println("3) Buscar Producto en Proveedor");
-            System.out.println("4) Lista de Productos del Proveedor");
+            System.out.println("1) Agregar Producto a Proveedor");
+            System.out.println("2) Eliminar Producto a Proveedor");
+            System.out.println("3) Buscar Producto de Proveedor");
+            System.out.println("4) Lista de Productos de Proveedor");
             System.out.println("5) Mostrar Productos y su stock");
             System.out.println("6) Salir");
             System.out.println("=============================================================");
@@ -54,19 +103,19 @@ public class GestorInventarioSupermercado {
             switch (opcion) {
                 case 1:
                     // ================================
-                    // Agregar Proveedor
+                    // Agregar Producto a Proveedor
                     // ================================
 
                     break;
                 case 2:
                     // ================================
-                    // Eliminar Proveedor
+                    // Eliminar Proveedor a Proveedor
                     // ================================
 
                     break;
                 case 3:
                     // ================================
-                    // Buscar Proveedor
+                    // Buscar Producto de Proveedor
                     // ================================
 
                     break;
@@ -85,8 +134,15 @@ public class GestorInventarioSupermercado {
                 case 6:
                     // ========================================
                     // Salir
-                    // ========================================                    
-                    break;
+                    // ========================================
+                    
+                    System.out.println("Saliendo...");
+
+                    pausar();
+
+                    lector.close();
+
+                    System.exit(0);
                 default:
                     System.out.println("Opción inválida.");
                     break;
@@ -94,8 +150,8 @@ public class GestorInventarioSupermercado {
 
             pausar();
 
-        } while (opcion != 6);
-
-        lector.close();
+            // Se limpia el buffer
+            lector.readLine();
+        }
     }
 }
